82a0be0f5e8d0e9ba5f8c723609d1a51f07af017
wangwren <wangwren0906@gmail.com>
2026-01-14 16:29:58 +0800

    feat(# NEW-0001): 腾讯云cos

--- mall-third-party/src/main/java/com/core/mall/thirdparty/MallThirdPartyApplication.java
+++ mall-third-party/src/main/java/com/core/mall/thirdparty/MallThirdPartyApplication.java
@@ -2,9 +2,11 @@ package com.core.mall.thirdparty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = "com.core.mall")
+@SpringBootApplication(scanBasePackages = "com.core.mall"
+        , exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class MallThirdPartyApplication {
 
